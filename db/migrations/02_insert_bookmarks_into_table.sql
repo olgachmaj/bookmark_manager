69d6db67a1cb5e61dbd12728f59d47c01473706d
Katie Whitlock <katiejwhitlock@gmail.com>
2021-02-23 15:03:30 +0000

    Bookmarks inserted and instructions added to db migration

---
+++ db/migrations/02_insert_bookmarks_into_table.sql
@@ -0,0 +1,10 @@
+Run the four queries below in `psql postgres`
+
+INSERT INTO bookmarks (url) VALUES
+('http://www.makersacademy.com');
+INSERT INTO bookmarks (url) VALUES
+ ('http:/www.destroyallsoftware.com');
+INSERT INTO bookmarks (url) VALUES
+('http://www.twitter.com');
+INSERT INTO bookmarks (url) VALUES
+ ('http://www.google.com');
